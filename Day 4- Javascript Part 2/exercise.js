86cf23587b800433ea51545885a3c30a28011419
edmondchoi <superstar.ed@gmail.com>
2024-02-26 21:51:40 -0800

    Day 4 JS

---
+++ Day 4- Javascript Part 2/exercise.js
@@ -0,0 +1,13 @@
+function checkEvenOrOdd(num) {
+    if (num%2 == 0) {
+       return "Even";
+   }  else {
+       return "Odd";
+   }
+   }
+   
+   let num1 = 20;
+   let num2 = 25;
+   console.log('The num1 is ', checkEvenOrOdd(num1));
+   console.log('The num2 is ', checkEvenOrOdd(num2));
+   
