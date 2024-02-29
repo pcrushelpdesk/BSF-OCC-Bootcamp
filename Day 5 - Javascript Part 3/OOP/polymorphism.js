a8cae0449294fbae0f7554409e659a0ee6a577f9
edmondchoi <superstar.ed@gmail.com>
2024-02-28 22:04:04 -0800

    Day 5 - more JS

---
+++ Day 5 - Javascript Part 3/OOP/polymorphism.js
@@ -0,0 +1,25 @@
+//Polymorphism
+
+class Animal {
+    makeSound() {
+        return `The animal makes a noise.`
+    }
+}
+
+class Dog extends Animal {
+    makeSound() {
+        return super.makeSound() + " I heard a dog barked."
+    }
+}
+
+class Cat  extends Animal {
+    makeSound() {
+        return super.makeSound() + ' I heard a cat Meoow.'
+    }
+}
+
+const dog = new Dog();
+console.log(dog.makeSound());
+
+const cat = new Cat();
+console.log(cat.makeSound());
